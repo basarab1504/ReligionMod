d7b45c2ca2f955c64815c4c56ac612c11f9d6c3f
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2018-12-14 23:46:04 +0300

    DoSomeWork

---
+++ Source/Religion/Religion/RoomRoleWorker_Church.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace Religion
+{
+    class RoomRoleWorker_Church : RoomRoleWorker
+    {
+        public override float GetScore(Room room)
+        {
+            int num = 0;
+            List<Thing> andAdjacentThings = room.ContainedAndAdjacentThings;
+            for (int index = 0; index < andAdjacentThings.Count; ++index)
+            {
+                Thing thing = andAdjacentThings[index];
+                if (thing.def.category == ThingCategory.Building && thing.def.defName == "Altar")
+                    ++num;
+            }
+            return (float)num * 7.6f;
+        }
+    }
+}
