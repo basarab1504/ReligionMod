ade930ede4c3dcf144f28fdad1ec17bea49b98ed
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2018-12-15 00:47:38 +0300

    Base Altar

--- Source/Religion/Religion/RoomRoleWorker_Church.cs
+++ Source/Religion/Religion/RoomRoleWorker_Church.cs
@@ -8,19 +8,19 @@ using Verse;
 
 namespace Religion
 {
-    class RoomRoleWorker_Church : RoomRoleWorker
-    {
-        public override float GetScore(Room room)
-        {
-            int num = 0;
-            List<Thing> andAdjacentThings = room.ContainedAndAdjacentThings;
-            for (int index = 0; index < andAdjacentThings.Count; ++index)
-            {
-                Thing thing = andAdjacentThings[index];
-                if (thing.def.category == ThingCategory.Building && thing.def.defName == "Altar")
-                    ++num;
-            }
-            return (float)num * 7.6f;
-        }
-    }
+    //class RoomRoleWorker_Church : RoomRoleWorker
+    //{
+    //    public override float GetScore(Room room)
+    //    {
+    //        int num = 0;
+    //        List<Thing> andAdjacentThings = room.ContainedAndAdjacentThings;
+    //        for (int index = 0; index < andAdjacentThings.Count; ++index)
+    //        {
+    //            Thing thing = andAdjacentThings[index];
+    //            if (thing.def.minifiedDef)
+    //                ++num;
+    //        }
+    //        return (float)num * 7.6f;
+    //    }
+    //}
 }
