ee76cbc522e2e7860ece79480e500f1f3168ca94
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2018-12-15 01:49:05 +0300

    Absolute Base Altar

---
+++ Source/Religion/Religion/ReligionDefOf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace Religion
+{
+    [DefOf]
+    class ReligionDefOf
+    {
+        //public static ThingDef Altar;
+    }
+}
