3775121e6810db699ae54648cd86ad688af4484d
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2018-12-17 03:34:43 +0300

    NoChurch Thought added

---
+++ Source/Religion/Religion/ThoughtDef_AltarThought.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace Religion
+{
+    public class ThoughtDef_AltarThought : ThoughtDef
+    {
+        public ThingDef altar;
+    }
+}
---
+++ Source/Religion/Religion/ThoughtWorker_NoChurch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace Religion
+{
+    public class ThoughtWorker_NoChurch : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            ThoughtDef_AltarThought relDef = this.def as ThoughtDef_AltarThought;
+            if (!p.Map.listerBuildings.ColonistsHaveBuilding(relDef.altar))
+                return (ThoughtState)true;
+            return ThoughtState.Inactive;
+        }
+    }
+}
