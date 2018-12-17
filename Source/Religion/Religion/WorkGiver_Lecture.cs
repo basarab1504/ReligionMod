ffef217f42858eea9953c2e54b30678492082d7d
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2018-12-17 20:11:42 +0300

    Add Lecture work

--- Source/Religion/Religion/WorkGiver_Lecture.cs
+++ Source/Religion/Religion/WorkGiver_Lecture.cs
@@ -5,11 +5,35 @@ using System.Text;
 
 using RimWorld;
 using Verse;
+using Verse.AI;
 
 namespace Religion
 {
     class WorkGiver_Lecture : WorkGiver_Scanner
     {
+        public override ThingRequest PotentialWorkThingRequest
+        {
+            get
+            {
+                if (this.def.fixedBillGiverDefs != null && this.def.fixedBillGiverDefs.Count == 1)
+                    return ThingRequest.ForDef(this.def.fixedBillGiverDefs[0]);
+                return ThingRequest.ForGroup(ThingRequestGroup.PotentialBillGiver);
+            }
+        }
 
+        public override PathEndMode PathEndMode
+        {
+            get { return PathEndMode.OnCell; }
+        }
+
+        public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            return true;
+        }
+
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            return new Job(ReligionDefOf.HoldLecture, (LocalTargetInfo)t);
+        }
     }
 }
