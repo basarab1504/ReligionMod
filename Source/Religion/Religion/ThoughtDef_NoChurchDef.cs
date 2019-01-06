e098cb44c68931af4289c5e5436e17f208ddf978
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2019-01-06 15:45:19 +0300

    fixes?

---
+++ Source/Religion/Religion/ThoughtDef_NoChurchDef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Religion
+{
+    class ThoughtDef_NoChurchDef : ThoughtDef
+    {
+        //public ThoughtDef_NoChurchDef()
+        //{
+
+        //}
+    }
+}
--- Source/Religion/Religion/ThoughtWorker_NoChurch.cs
+++ Source/Religion/Religion/ThoughtWorker_NoChurch.cs
@@ -12,13 +12,16 @@ namespace Religion
     {
         protected override ThoughtState CurrentStateInternal(Pawn p)
         {
-            if (!p.Map.listerBuildings.ColonistsHaveBuilding(ReligionDefOf.Altar))
-                return (ThoughtState)true;
-            IEnumerable<Building> altars = p.Map.listerBuildings.AllBuildingsColonistOfDef(ReligionDefOf.Altar);
-            foreach (Building_Altar a in altars)
-                    if (a.religion.Contains(def.requiredTraits[0]))
+            if (p.story.traits.allTraits.Find(x => x.def is TraitDef_ReligionTrait) != null)
+            {
+                TraitDef pawnReligion = p.story.traits.allTraits.Find(x => x.def is TraitDef_ReligionTrait).def;
+                IEnumerable<Building> altars = p.Map.listerBuildings.AllBuildingsColonistOfDef(ReligionDefOf.Altar);
+                foreach (Building_Altar a in altars)
+                    if (a.religion.Contains(pawnReligion))
                         return (ThoughtState)false;
-            return (ThoughtState)true;
+                return (ThoughtState)true;
+            }
+            return (ThoughtState)false;
         }
     }
 }
